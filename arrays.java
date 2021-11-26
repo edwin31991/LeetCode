0ff5ecd34669f7fa5ab3f4973e24666ed0ba1c88
edwin dalmeida <edwin31991@gmail.com>
2021-11-26 19:03:45 +0530

    initial commit

---
+++ arrays.java
@@ -0,0 +1,38 @@
+Given an integer array nums sorted in non-decreasing order, return an array of the squares of each number sorted in non-decreasing order
+
+class Solution {
+    public int[] sortedSquares(int[] A) {
+        int N = A.length;
+        int[] ans = new int[N];
+        for (int i = 0; i < N; ++i)
+            ans[i] = A[i] * A[i];
+
+        Arrays.sort(ans);
+        return ans;
+    }
+}
+
+
+You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+
+
+
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        for (int i = 0; i < n; i++) {
+            nums1[i + m] = nums2[i];
+        }
+        Arrays.sort(nums1);
+    }
+}
+
+THIS IS A PROGRAM TO MERGE TWO ARRAYS
+
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        for (int i = 0; i < n; i++) {
+            nums1[i + m] = nums2[i];
+        }
+        Arrays.sort(nums1);
+    }
+}
